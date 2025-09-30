9454b90480c2d32f4dcef0b9c69b407cba2f38bd
Badrio1707 <badrio.yt.gaming@gmail.com>
2025-09-30 11:42:23 +0700

    Create Expo With Nativewind

---
+++ app/index.jsx
@@ -0,0 +1,10 @@
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function Index() {
+  return (
+    <SafeAreaView className="flex-center">
+      <Text className="text-red-500">Edit app/index.tsx to edit this screen.</Text>
+    </SafeAreaView>
+  );
+}
